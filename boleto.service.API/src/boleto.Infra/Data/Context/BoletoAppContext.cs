786402db8ec617f342ce3665ae508dc1de89e9ef
Marcelo Gonçalves <marcelojunior_@outlook.com.br>
2025-12-28 22:13:33 -0300

    feat: adiciona contexto do banco de dados e referências de projeto #5

---
+++ boleto.service.API/src/boleto.Infra/Data/Context/BoletoAppContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace boleto.Infra.Data.Context;
+
+public class BoletoAppContext : DbContext
+{
+    // TODO: Adicionar DbSets aqui, por exemplo:
+    // public DbSet<YourEntity> YourEntities { get; set; }
+
+    public BoletoAppContext(DbContextOptions<BoletoAppContext> options) : base(options) { }
+}
