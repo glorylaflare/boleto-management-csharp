46b4926da741ba2634b128a4d651a36c614eb6bb
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-12-29 21:21:32 -0300

    feat: Create ValueObjects

---
+++ boleto.service.API/src/boleto.Domain/ValueObjects/Banco.cs
@@ -0,0 +1,18 @@
+﻿namespace boleto.Domain.ValueObjects;
+
+public class Banco
+{
+    
+    public string Codigo { get; set; }
+    public string Nome { get; set; }
+    
+    public void ValidaLinhaDigitavel(string linhaDigitavel)
+    {
+        // Implementar a lógica de validação da linha digitável específica para este banco
+    }
+    
+    public void ValidaCodigoBarras(string codigoBarras)
+    {
+        // Implementar a lógica de validação do código de barras específica para este banco
+    }
+}
---
+++ boleto.service.API/src/boleto.Domain/ValueObjects/Money.cs
@@ -0,0 +1,25 @@
+﻿namespace boleto.Domain.ValueObjects;
+
+public class Money
+{
+    
+    public decimal Valor { get; set; }
+    public string Moeda { get; set; } = "BRL";
+
+
+    public void Somar(Money outro)
+    {
+        if (outro.Valor > 0)
+        {
+            Valor += outro.Valor;
+        }
+    }
+    
+    public void Subtrair(Money outro)
+    {
+        if (outro.Valor > 0)
+        {
+            Valor -= outro.Valor;
+        }
+    }
+}
