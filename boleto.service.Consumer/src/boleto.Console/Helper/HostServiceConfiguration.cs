9dd94d1b8f0c886559ab5e54b575772e1da2c225
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-01-06 17:45:24 -0300

    feat: Add HostServiceConfiguration for setting up the host and services

---
+++ boleto.service.Consumer/src/boleto.Console/Helper/HostServiceConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boleto.Console.Helper
+{
+    public class HostServiceConfiguration
+    {
+        public static IHost HostConfiguration()
+        {
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices(services =>
+                {
+                    services.AddHostedService<Worker>();
+                })
+                .Build();
+            return host;
+        }
+    }
+}
